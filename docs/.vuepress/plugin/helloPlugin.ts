78d64928d80e424e115a8643476a37c5882ece4c
yzqdev <yzqdev@outlook.com>
2024-04-26 18:35:35 +0800

    hello pllugin

---
+++ docs/.vuepress/plugin/helloPlugin.ts
@@ -0,0 +1,28 @@
+import { createPage, type Page, type PluginFunction } from "vuepress/core";
+export const helloPlugin =
+  (options?:any): PluginFunction =>
+  (app) => {
+    console.log("hello");
+    return {
+      name: "hello",
+      async onInitialized(app) {
+        // 如果主页不存在
+
+        // 创建一个主页
+        const helloPage = await createPage(app, {
+          path: "/hello",
+          // 设置 frontmatter
+          frontmatter: { title: "hello" },
+          // 设置 markdown 内容
+          content: `\
+# 欢迎来到 ${app.options.title}
+
+这是 hello
+`,
+        });
+        // 把它添加到 `app.pages`
+        app.pages.push(helloPage);
+        console.log(app.pages);
+      },
+    };
+  };
